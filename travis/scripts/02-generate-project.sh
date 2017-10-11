af6bea3ba378e9ddab87c511cdbe4cf5e792b463
cbornet <cbornet@hotmail.com>
2017-10-11 12:44:42 +0000

    Test with generator-jhipster master

--- travis/scripts/02-generate-project.sh
+++ travis/scripts/02-generate-project.sh
@@ -16,6 +16,7 @@ if [ "$JHIPSTER" == "app-ng2-gateway-uaa" ]; then
     mkdir -p "$UAA_APP_FOLDER"
     cp -f "$JHIPSTER_SAMPLES"/uaa/.yo-rc.json "$UAA_APP_FOLDER"/
     cd "$UAA_APP_FOLDER"
+    yarn link generator-jhipster
     yo jhipster --force --no-insight --with-entities --skip-checks
     ls -al "$UAA_APP_FOLDER"
 fi
@@ -23,7 +24,8 @@ fi
 mkdir -p "$APP_FOLDER"
 cp -f "$JHIPSTER_SAMPLES"/"$JHIPSTER"/.yo-rc.json "$APP_FOLDER"/
 cd "$APP_FOLDER"
-yarn link generator-jhipster-grpc
+yarn link generator-jhipster
 yo jhipster --force --no-insight --skip-checks --with-entities
+yarn link generator-jhipster-grpc
 yo jhipster-grpc --force --all-entities
 ls -al "$APP_FOLDER"
