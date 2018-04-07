e8a70e807980f0cd9f3b241e0e47acef16969d02
zhanyi <zyzy257@gmail.com>
2018-04-07 20:18:07 +0800

    Fix: issue #18
    Can not generate the code, When the app haven't testFramework

--- generators/app/index.js
+++ generators/app/index.js
@@ -23,7 +23,7 @@ module.exports = class extends BaseGenerator {
             },
             displayLogo: function () {
                 this.log('Welcome to the ' + chalk.red('JHipster grpc') + ' generator! ' + chalk.yellow('v' + packagejs.version + '\n'));
-                if (this.jhipsterAppConfig.testFrameworks.includes('gatling')) {
+                if (this.jhipsterAppConfig.testFrameworks && this.jhipsterAppConfig.testFrameworks.includes('gatling')) {
                     this.log(chalk.red('Applications using Gatling are currently not supported (see https://github.com/cbornet/generator-jhipster-grpc/issues/4)'));
                     this.abort = true;
                 }
